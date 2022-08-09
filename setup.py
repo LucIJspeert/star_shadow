abcdbf5a7f305a640ade8c7af69b322f539d9483
luc_wl <46969344+LucIJspeert@users.noreply.github.com>
2022-08-09 11:35:41 +0200

    setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+"""STAR SHADOW
+
+Code written by: Luc IJspeert
+"""
+
+from setuptools import setup
+
+
+# package version
+MAJOR = 0
+MINOR = 1
+ATTR = '0'
+# full acronym
+ACRONYM = ('Satellite Time-series Analysis Routine using Sinusoids and Harmonics Automatedly '
+           'for Double stars with Occultations and Waves')
+
+setup(name="star_shadow",
+      version=f'{MAJOR}.{MINOR}.{ATTR}',
+      author='Luc IJspeert',
+      license='GNU General Public License v3.0',
+      description=ACRONYM,
+      long_description=open('README.md').read(),
+      packages=['star_shadow'],
+      package_dir={'star_shadow': 'star_shadow'},
+      package_data={'star_shadow': ['star_shadow/data/tess_sectors.dat']},
+      include_package_data=True,
+      install_requires=['numpy', 'scipy', 'numba', 'astropy', 'ellc', 'arviz', 'h5py', 'matplotlib', 'corner'])
