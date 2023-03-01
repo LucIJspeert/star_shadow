55a6b64511e51e36a7bb7459ae1e1413bb7747b8
luc_wl <46969344+LucIJspeert@users.noreply.github.com>
2023-03-01 19:38:06 +0100

    namespaces

--- star_shadow/__init__.py
+++ star_shadow/__init__.py
@@ -5,8 +5,9 @@ Code written by: Luc IJspeert
 
 from . import main_functions
 from .main_functions import *
-from . import timeseries_functions
-from . import timeseries_fitting
-from . import analysis_functions
-from . import utility
-from . import visualisation
+from . import timeseries_functions as tsf
+from . import timeseries_fitting as tsfit
+from . import analysis_functions as af
+from . import mcmc_functions as mcf
+from . import utility as ut
+from . import visualisation as vis
